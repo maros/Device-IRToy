555595b40b93c91fe602686d065210387ebd36ad
Maros Kollar <maros@k-1.com>
2015-05-17 18:41:33 +0200

    Turn on hifi program

---
+++ meta/turn_on_hifi.pl
@@ -0,0 +1,48 @@
+
+# SLEEP
+#    {
+#      'count' => 1,
+#      'length' => 10,
+#      'value' => 312,
+#      'module' => 6
+#    };
+
+# POWER
+#    {
+#          'count' => 1,
+#          'length' => 10,
+#          'value' => 40,
+#          'module' => 6
+#    };
+
+# TUNER
+#    {
+#          'count' => 1,
+#          'length' => 10,
+#          'value' => 616,
+#          'module' => 6
+#    };
+
+# FUNCTION
+#    {
+#          'count' => 1,
+#          'length' => 10,
+#          'value' => 1000,
+#          'module' => 6
+#    };
+
+# VOL+
+#    {
+#          'count' => 1,
+#          'length' => 10,
+#          'value' => 712,
+#          'module' => 6
+#    };
+
+# VOL-
+#    {
+#          'count' => 1,
+#          'length' => 10,
+#          'value' => 200,
+#          'module' => 6
+#    };
