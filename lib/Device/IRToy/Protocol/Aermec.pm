2250e47479d4dbf4fce45e056f149e8007e0b533
Maros Kollar <maros@k-1.com>
2016-09-26 07:26:51 +0200

    Working Aermec de & encocder

--- lib/Device/IRToy/Protocol/Aermec.pm
+++ lib/Device/IRToy/Protocol/Aermec.pm
@@ -1,73 +1,78 @@
 package Device::IRToy::Protocol::Aermec {
     use 5.016;
     use warnings;
-    
+
     use Device::IRToy::Utils;
     use List::Util qw(min);
-    
-    my @INIT = (9000,4500);
-    
+
+    my @INIT    = (9000,4500);
+    my $PERIOD  = 600;
+
     sub maxsignal {
-        return 22000;
+        return 40000;
     }
-    
+
     sub encode {
         my ( $class,$data ) = @_;
-        
+
         return
-            unless defined $data
-            && ref $data eq 'ARRAY';
-        
+            unless defined $data;
+
         my @encoded = @INIT;
-        foreach my $byte (@{$data}) {
-#            for (reverse(0..7)) {
-#                if ($byte & 2 ** $_) {
-#                    push(@encoded,360,1500);
-#                } else {
-#                    push(@encoded,360,550);
-#                }
-#            }
+        foreach my $bit (split //, unpack("b*",$data)) {
+            push(@encoded,600);
+            push(@encoded,$bit ? 1650 : 600);
         }
-        
+
         return \@encoded;
     }
-    
+
     sub decode {
         my ( $class,$data ) = @_;
-        
+
         return
             unless defined $data
-            && ref $data eq 'ARRAY';
-        
-        my $min     = min(@$data);
-        
-        for my $index (0..$#INIT) {
-            if ( !Device::IRToy::Utils::check_fuzzy(  $data->[$index], $INIT[$index], 1000 ) ) {
-                msg('WARN',"Probably not Panasonic! period %i:%%.4fµs %iµs(+/-1000) expected",$index,$INIT[$index]);
-                return;
+            && ref $data eq 'ARRAY'
+            && scalar(@{$data}) > 2;
+
+        my @process = @{$data};
+        my $valid   = 0;
+        my $check;
+        while ($valid < scalar @INIT
+            && scalar(@process)) {
+            $check = shift(@process);
+            if (Device::IRToy::Utils::check_fuzzy( $check, $INIT[$valid], 250 )) {
+                $valid++;
+            } else {
+                $valid = 0;
             }
         }
-        
-        my ($j,$i);
-        my @result = ();
-        
-        my $bytes = ( $#{$data} - 2 ) / 16;
-        for ($i = 0; $i < ( $#{$data} - 2 ) / 16; $i++ ) {
-            my $bit = 7;
-            $result[$i] = 0;
-            for ( $j = 0; $j < 16; $j += 2 ) {
-                $result[$i] |= 1 << ($bit) 
-                    if $data->[3+16*$i+$j] > ( $min * 2 );
-                $bit--;
+
+        unless ($valid == scalar @INIT) {
+            msg('WARN',"Probably not Aermec! period %i %iµs(+/-250) expected. Got %i",$valid,$INIT[$valid],$check);
+            return;
+        }
+
+        my @result;
+        my $pos = 0;
+        for (my $i = 0; $i <= $#process; $i++) {
+            my $len = $process[$i];
+            if ($len > 19_000) {
+                next;
+            } else {
+                if ($i % 2 == 1) {
+                    if (Device::IRToy::Utils::check_fuzzy( $len, 1650, 150 )) {
+                        push(@result,1);
+                    } elsif (Device::IRToy::Utils::check_fuzzy( $len, 600, 150 )) {
+                        push(@result,0);
+                    } else {
+                        msg('WARN','Invalid pause %i',$len);
+                    }
+                }
             }
         }
-        
-        my $check = 2+16*($i-1)+$j;
-        if ( $check != $#{$data} ) {
-            msg('WARN',"Stop bit missing: %i != %i",$check,$#{$data});
-        }
-        
-        return \@result;
+
+        return pack("b*", join("",@result));
     }
 }
 
